c2fb45a1b9495d49b1b33e9a74971b92aa80a04e
Andjar2 <andersonjjar@gmail.com>
2025-07-17 11:16:52 -0400

    node3

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['nbG0tJjg3lYfK1weLrHJsQ==', 'T5/XzkuqmBRYbHikm7x7hQ==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
